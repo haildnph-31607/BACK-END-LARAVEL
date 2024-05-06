6bc306b7fb2ba7256e923c6c22ffe762530e2a63
unknown <haildnph31607@fpt.edu.vn>
2024-05-07 01:02:22 +0700

    firt

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('avatar')->nullable();
             $table->string('username')->nullable();
-           
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            "username" => "admin",
+            "name" => "Admin",
+            "email" => "nguyenminhkhong2004@gmail.com",
+            "password" => Hash::make("haitoi2004"),
+            "departments_id" => "1",
+            "status_id" => "1"
+
+        ]);
+    }
+}
